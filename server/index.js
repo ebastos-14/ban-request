ec21fe8a810fac82157a1fba1bbabd008f878b37
EdinsonFabianBastosCamacho <54146067+ebastos-14@users.noreply.github.com>
2026-06-01 23:39:12 -0500

    Integrate Twitch client and chat event handling

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import tmi from "tmi.js";
 
 import { CONFIG } from "./config.js";
 import { getChannel } from "./state.js";
@@ -6,6 +7,62 @@ import { getChannel } from "./state.js";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+/*
+|--------------------------------------------------------------------------
+| TWITCH CLIENT
+|--------------------------------------------------------------------------
+*/
+
+const twitchClient = new tmi.Client({
+
+    identity: {
+
+        username: CONFIG.BOT_USERNAME,
+
+        password: CONFIG.OAUTH_TOKEN
+
+    },
+
+    channels: CONFIG.ALLOWED_CHANNELS
+
+});
+
+twitchClient.connect()
+.then(() => {
+
+    console.log("Connected to Twitch");
+
+})
+.catch(error => {
+
+    console.error("Twitch connection error:", error);
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| TWITCH CHAT EVENTS
+|--------------------------------------------------------------------------
+*/
+
+twitchClient.on("message", (channel, tags, message, self) => {
+
+    if (self) return;
+
+    console.log({
+        channel,
+        user: tags.username,
+        message
+    });
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| API ROUTES
+|--------------------------------------------------------------------------
+*/
 
 app.get("/", (req, res) => {
 
@@ -38,6 +95,12 @@ app.get("/event", (req, res) => {
     res.json(channelData);
 
 });
+
+/*
+|--------------------------------------------------------------------------
+| SERVER START
+|--------------------------------------------------------------------------
+*/
 
 app.listen(PORT, () => {
 
